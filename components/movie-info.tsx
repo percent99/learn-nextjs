d5960063386ab60a1bae3e30ec802151836086ab
Yeongjin Jeong <yj_jeong@percent99.co.kr>
2025-02-18 13:09:24 +0900

    movie commit.

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/movie-info.module.css';
 import { API_URL } from "../app/(home)/page"
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   return await fetch(`${API_URL}/${id}`).then(res => res.json())
 }
 
