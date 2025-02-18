17d363d67bb30a118f5f88d47b74b06ea5916510
Yeongjin Jeong <yj_jeong@percent99.co.kr>
2025-02-18 12:30:25 +0900

    commit.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next"
-import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from '../../styles/home.module.css';
 
 export const metadata: Metadata = {
   title: "Home",
@@ -18,11 +19,9 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies()
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map(movie => 
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie key={movie.id} id={movie.id} title={movie.title} poster_path={movie.poster_path} />
       )}
     </div>
   )
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -5,11 +5,9 @@ import MovieVideos from "../../../../components/movie-videos";
 export default async function MovieDetail({ params: {id}, }: { params: {id: string}}) {
   return (
     <div>
-      <h3>Movie Detail Page.</h3>
       <Suspense fallback={<h1>Loading Movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <h4>Videos</h4>
       <Suspense fallback={<h1>Loading Movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
