ea5eedde3e5d1f5b423f120db394548a5bb50f83
Yeongjin Jeong <yj_jeong@percent99.co.kr>
2025-02-18 14:44:04 +0900

    params 및 searchParams가 비동기식으로 처리되는 방식으로 전환 문제 해결

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,21 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-  params: { id: string }
-}
+// interface IParams {
+//   params: Promise<{ id: string }>
+// }
+type PageParams = Promise<{ id: string }>
 
-export async function generateMetadata({ params: {id}, }: IParams) {
+export async function generateMetadata({ params }: { params: PageParams}) {
+  const { id } = await params
   const movie = await getMovie(id)
   return {
     title: movie.title,
   }
 }
 
-export default async function MovieDetail({ params: {id}, }: IParams) {
+export default async function MovieDetail({ params }: { params: PageParams}) {
+  const { id } = await params
   return (
     <div>
       <Suspense fallback={<h1>Loading Movie info</h1>}>
